06022ceb86eab03fc264b5a65155b00ca236866e
Marcio Junior <marciohamer@gmail.com>
2025-03-04 14:40:20 -0300

    Start dockering

--- app/src/services/api.tsx
+++ app/src/services/api.tsx
@@ -1,31 +1,4 @@
 import axios from 'axios';
-const usuariosMock = [
-  { id: 1, nome: "Alice" },
-  { id: 2, nome: "Bob" },
-  { id: 3, nome: "Carlos" },
-];
-
-const noticiasMock = [
-  {
-    id_noticia: "23kj2l3123klj9djladdsds234g",
-    titulo: "Notícia 1",
-    subtitulo: "Subtítulo da Notícia 1",
-    url: "www.noticia.com.br/noticia1",
-  },
-  {
-    id_noticia: "98asd7f98a7sdf89a7sdf89a7sd",
-    titulo: "Notícia 2",
-    subtitulo: "Subtítulo da Notícia 2",
-    url: "www.noticia.com.br/noticia2",
-  },
-  {
-    id_noticia: "12kj3h1k2j3h1k2j3h1k2j3h1k2j",
-    titulo: "Notícia 3",
-    subtitulo: "Subtítulo da Notícia 3",
-    url: "www.noticia.com.br/noticia3",
-  },
-];
-
 
 let api_url = process.env.API; 
 
